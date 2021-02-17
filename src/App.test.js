cb6f8a91202d422dfd8a7d65b1f797509561dd63
Allwyn Selvin <allwynjebahar@gmail.com>
2021-02-18 00:31:03 +0530

    test case fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
+
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+test('renders learn react link', () => {
+  render(<App />);
+  // const linkElement = screen.getByText(/Welcome to React/i);
+  // console.log(linkElement);
+  expect(screen.getByText(/Welcome to React/i)).toBeTruthy();
 });
+
